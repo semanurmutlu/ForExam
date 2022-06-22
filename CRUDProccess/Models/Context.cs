6b40abb47f7db3c9dd04b5be33bd512618c0bf39
Semanurmutlu <58692989+semanurmutlu@users.noreply.github.com>
2022-06-22 22:37:59 +0300

    Add Student.cs and Context.cs

---
+++ CRUDProccess/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDProccess.Models
+{
+    public class Context :DbContext
+    {
+        //Nuget olarak entity yükledim sonra :DbContext ekleme yapıyorum
+
+        public Context(DbContextOptions<Context>options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
---
+++ CRUDProccess/Models/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDProccess.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [StringLength(20)]
+        public string Surname { get; set; }
+
+        [StringLength(11)]
+        public string TC { get; set; }
+
+        [StringLength(50)]
+        public string Address  { get; set; }
+
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [StringLength(20)]
+        public string Education { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; }
+
+    }
+}
